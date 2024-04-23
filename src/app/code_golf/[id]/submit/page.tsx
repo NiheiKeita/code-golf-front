f1709b1a0935c848288a5b010099f267c8aae993
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-04-23 23:08:45 +0900

    修正

--- src/app/code_golf/[id]/submit/page.tsx
+++ src/app/code_golf/[id]/submit/page.tsx
@@ -2,7 +2,7 @@ import { CodeSubmitView } from "@/views/CodeSubmitView";
 import React from "react";
 
 const CountPage = React.memo(() => {
-  return <CodeSubmitView id="ss" />;
+  return <CodeSubmitView />;
 });
 
 export default CountPage;
--- src/views/CodeSubmitView/index.tsx
+++ src/views/CodeSubmitView/index.tsx
@@ -1,15 +1,9 @@
 import React from "react"
 
-type Props = {
-  id: string,
-}
-
-export const CodeSubmitView = React.memo<Props>(function CodeSubmitView({
-  id,
-},) {
+export const CodeSubmitView = React.memo(function CodeSubmitView() {
   return (
     <div className="w-fit rounded bg-red-500 px-4 py-2 text-white hover:bg-red-300">
-      View {id}
+      View
     </div>
   )
 })
