e889fcbbdd68f77c5af054e788fd04970616ae1a
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-04-29 00:24:29 +0900

    修正

--- src/views/PushView/index.tsx
+++ src/views/PushView/index.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { onBackgroundMessage } from "firebase/messaging/sw";
 import React, { useEffect } from "react"
 const apiKey: string | undefined = process.env.NEXT_PUBLIC_APP_API_KEY ?? ''
 const authDomain: string | undefined = process.env.NEXT_PUBLIC_AUTH_DOMAIN ?? ''
+const databaseURL: string | undefined = process.env.NEXT_PUBLIC_DATABASE_URL ?? ''
 const projectId: string | undefined = process.env.NEXT_PUBLIC_PROJECT_ID ?? ''
 const storageBucket: string | undefined = process.env.NEXT_PUBLIC_STORAGE_BUCKET ?? ''
 const messagingSenderId: string | undefined = process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID ?? ''
@@ -17,6 +19,7 @@ export const PushView = React.memo(function PushView() {
     const firebaseConfig = {
       apiKey: apiKey,
       authDomain: authDomain,
+      databaseURL: databaseURL,
       projectId: projectId,
       storageBucket: storageBucket,
       messagingSenderId: messagingSenderId,
@@ -27,8 +30,36 @@ export const PushView = React.memo(function PushView() {
     const app = initializeApp(firebaseConfig)
     const messaging = getMessaging(app)
     const analytics = getAnalytics(app)
-    console.log(projectId)
-    getToken(messaging, { vapidKey: vapidKey });
+    console.log(appId)
+    // getToken(messaging, { vapidKey: vapidKey });
+    getToken(messaging, { vapidKey: vapidKey }).then((currentToken) => {
+      if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        console.log('ddd');
+        console.log(currentToken);
+        // ...
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
+    });
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+    });
+    // function requestPermission() {
+    //   console.log('Requesting permission...');
+    //   Notification.requestPermission().then((permission) => {
+    //     if (permission === 'granted') {
+    //       console.log('Notification permission granted.');
+    //     }
+    //   })
+    // }
+    // requestPermission()
+
   }, [])
   return (
     <>
