b86169038f313c367d02e0d84062589fab4b3b31
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-05-02 19:01:05 +0900

    修正（Pushは来る）

--- src/views/PushView/index.tsx
+++ src/views/PushView/index.tsx
@@ -1,9 +1,7 @@
 "use client"
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
-import { onBackgroundMessage } from "firebase/messaging/sw";
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 const apiKey: string | undefined = process.env.NEXT_PUBLIC_APP_API_KEY ?? ''
 const authDomain: string | undefined = process.env.NEXT_PUBLIC_AUTH_DOMAIN ?? ''
 const databaseURL: string | undefined = process.env.NEXT_PUBLIC_DATABASE_URL ?? ''
@@ -26,40 +24,27 @@ export const PushView = React.memo(function PushView() {
       appId: appId,
       measurementId: measurementId,
     }
+    // console.log(projectId)
 
     const app = initializeApp(firebaseConfig)
     const messaging = getMessaging(app)
-    const analytics = getAnalytics(app)
-    console.log(appId)
-    // getToken(messaging, { vapidKey: vapidKey });
+    // const analytics = getAnalytics(app)
+
     getToken(messaging, { vapidKey: vapidKey }).then((currentToken) => {
       if (currentToken) {
-        // Send the token to your server and update the UI if necessary
-        console.log('ddd');
+        console.log('-----currentToken-----');
         console.log(currentToken);
-        // ...
       } else {
-        // Show permission request UI
         console.log('No registration token available. Request permission to generate one.');
-        // ...
       }
     }).catch((err) => {
       console.log('An error occurred while retrieving token. ', err);
-      // ...
     });
+
     onMessage(messaging, (payload) => {
       console.log('Message received. ', payload);
     });
-    // function requestPermission() {
-    //   console.log('Requesting permission...');
-    //   Notification.requestPermission().then((permission) => {
-    //     if (permission === 'granted') {
-    //       console.log('Notification permission granted.');
-    //     }
-    //   })
-    // }
-    // requestPermission()
-
+    
   }, [])
   return (
     <>
