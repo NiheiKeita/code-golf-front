44d666d28232efd6043bb84e2d4d07ab8a3a77b2
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-05-08 00:18:14 +0900

    APIの処理分離

---
+++ src/views/QuestionListView/hooks.ts
@@ -0,0 +1,28 @@
+import { Question } from "@/types/Question";
+import { useCallback, useState } from "react";
+
+export const useGetQuestionList = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [questions, setQuestions] = useState<Question[]>();
+    const getQuestionList = useCallback(async () => {
+        setIsLoading(true)
+        await fetch('http://localhost:8081/questions', {
+            mode: 'cors'
+        })
+            .then(res => res.json())
+            .then(questions => {
+                setIsLoading(false)
+                questions.map((question: any) => {
+                    const id = question.id
+                    const title = question.title
+                    const detail = question.detail
+                    const exampleCode = question.exampleCode
+                    return { id, title, detail, exampleCode }
+                })
+                setQuestions(questions)
+            }).catch(() => {
+                setIsLoading(false)
+            })
+    }, [])
+    return { isLoading, getQuestionList, questions }
+}
--- src/views/QuestionListView/index.tsx
+++ src/views/QuestionListView/index.tsx
@@ -1,81 +1,25 @@
-import { Question } from "@/types/Question"
-import React, { useCallback, useEffect, useState } from "react"
+
+import React, { useEffect } from "react"
 import { Card } from "./components/Card"
+import { useGetQuestionList } from "./hooks"
 
-const questions: Question[] = [
-  {
-    id: 1,
-    title: "question1",
-    detail: "いわゆる「FizzBuzz」を実装してください。 1から100までの数字について、その数が3の倍数なら「Fizz」、5の倍数なら「Buzz」、15の倍数なら「FizzBuzz」、それ以外ならその数そのものを出力してください。それぞれの出力は改行で区切ってください。",
-    exampleCode: 'for ($i = 1; $i <= 100; $i++) {/n   echo match([$i % 3 === 0, $i % 5 === 0]) {/n    [true, true] => "FizzBuzz",/n      [true, false] => "Fizz",/n        [false, true] => "Buzz",/n         [false, false] => $i,/n      }, PHP_EOL;/n    }',
-  },
-  {
-    id: 2,
-    title: "question2",
-    detail: "RFC 4648 で定義された Base32 エンコーディングを実装してください。 標準入力から与えられる各行に対し、Base32 エンコードをおこなった文字列を標準出力へ改行区切りで出力してください。 なお、アルファベットの出力には大文字を用いてください。",
-    exampleCode: 'question2の例のコード',
-  },
-]
-const useGetData = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState<any>();
-
-  const getImages = useCallback(() => {
-    setIsLoading(true);
-    fetch('http://localhost:8081/questions')
-      .then(res => res.text())
-      .then(data => {
-        console.log(data)
-        setIsLoading(false)
-        setData(data)
-        console.log("ok")
-      }).catch(() => {
-        console.log("ng")
-        setIsLoading(false)
-      })
-
-  }, [])
-  return { isLoading, getImages, data }
-}
 
 export const QuestionListView = React.memo(function QuestionListView() {
+  const { isLoading, getQuestionList, questions } = useGetQuestionList()
 
-  // const { isLoading, getImages, data } = useGetData()
-  // // console.log(isLoading ? "d" : "ddd")
-  // useEffect(() => {
-  //   getImages()
-  // }, [getImages])
-  // const onSubmit = async (): Promise<any> => {
-  //   try {
-  //     const res = await fetch("http://localhost:8081/questions")
-  //       .then(res => {
-  //         console.log("ok")
-  //       })
-  //       .then(text => {
-  //         console.log("ok")
-  //       })
-  //   } catch (err) {
-  //     alert(err)
-  //   }
-  // }
-
-  const fetchUsers = async () => {
-    const response = await fetch('http://localhost:8081/questions', {
-      mode: 'cors'
-    })
-    const data = await response.json()
-    console.log(data)
-  }
+  useEffect(() => {
+    getQuestionList()
+  }, [getQuestionList])
   return (
     <>
       <div className="">
         <p className="my-10 text-5xl font-bold">
           問題一覧画面
         </p>
-        <button onClick={fetchUsers} >ddd</button>
+        {isLoading && <>ローディング</>}
         <div className="w-full px-10">
           {
-            questions.map((question, index) => {
+            questions?.map((question, index) => {
               return <Card key={index} link={`/questions/${encodeURIComponent(question.id)}`} body={question.title} />
             })
           }
